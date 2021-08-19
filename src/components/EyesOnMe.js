c21a54f538ffa942d4cd5e69fb26fe3fea18599d
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-08-19 14:37:59 -0500

    commit tree b1d57a9fa78b4c621e744a7e90f15dc2e4182ec4

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+ focus = (event) => {
+     console.log('Good!')
+ }
+
+ blur = (event) => {
+    console.log('Hey! Eyes on me!')
+ }
+
+ render() {
+    return (
+      <div>
+           <button onFocus={this.focus} onBlur={this.blur}>
+            My Buddy
+            </button>
+      </div>  
+       
+    )
+ }
+}
+
